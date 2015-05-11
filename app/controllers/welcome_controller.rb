75a492d30f2f0514162d41ca688124012fe0e683
ivan <peij@163.com>
2015-05-11 23:48:36 +0800

    css

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 #encoding: utf-8
 class WelcomeController < ApplicationController
   def index
-
+    redirect_to 'http://shop2161991.koudaitong.com/v2/feature/wwxhk8nw?common%2Furl%2Fcreate=&feature%2Fwwxhk8nw=&scan=3&from=groupmessage&isappinstalled=0'
   end
 
   def h513
