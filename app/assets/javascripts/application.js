dcdc178b9912085c163aca0d0f3bc7297743215d
ivan <peij@163.com>
2015-05-11 10:54:58 +0800

    first

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,4 @@
+//= require jquery.min
+//= require ishoock.tools
+//= require jweixin-1.0.0
+//= require questions.js
---
+++ app/assets/javascripts/questions.js
@@ -0,0 +1,17 @@
+/**
+ * Created by ivan on 15/5/9.
+ */
+
+var questions = [
+    { question: "鹅鹅鹅，曲项向天歌。是谁写的？", anwser: "B" },
+    { question: "癞蛤蟆想吃天鹅肉出字哪里？", anwser: "B" },
+    { question: "以下哪部是中国十大古典悲剧？", anwser: "B" },
+    { question: "千里送鹅毛,礼轻情意重，是谁说的？", anwser: "A" },
+    { question: "下面哪种动物与鸿雁是近亲？", anwser: "B" },
+
+    { question: "下面谁没有参加鹅湖之会？", anwser: "B" },
+    { question: "《雪夜喜李郎中见访》中出现的成语是？", anwser: "B" },
+    { question: "天鹅湖的作者是？", anwser: "B" },
+    { question: "雪颈霜毛红网掌，的后一句是？", anwser: "B" },
+    { question: "鹅换黄庭经”的是谁？", anwser: "A" }
+];
