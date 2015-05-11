d2b03f6f8ab7fa118c1350ac56e36ea0d978a64a
ivan <peij@163.com>
2015-05-11 15:35:02 +0800

    index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,8 @@ class WelcomeController < ApplicationController
   def index
 
   end
+
+  def h513
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root 'welcome#index'
+  root 'welcome#index'
 
-  get '/513', to: 'welcome#index'
+  get '/513', to: 'welcome#h513'
 end
